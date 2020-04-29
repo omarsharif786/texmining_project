8caa24516a7889535d66281a8a90377375d3cd86
Omar Sharif <omar.cse@hotmail.com>
2020-04-29 03:19:46 -0500

    Add files via upload

---
+++ readme_project.txt
@@ -0,0 +1,25 @@
+Python-Script is written for Text-minning i.e "textmining.py"
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+The Script was written in Windows 10 machine using Anaconda Library.
+
+# Create a Input Folder "texts" and place keep all txt files
+
+# To run the Program use following command ( in command line )
+ 
+	source('yourpath/textmining.py')
+
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+Input Files:
+	1. all-topics-strings.lc.txt
+	2. big.txt
+	3. cat-descriptions_120396.txt
+	4. feldman-cia-worldfactbook-data.txt
+
+Output Plots:
+	1. FrequencyHistogram.png
+	2. WordFrequencies.png
+	3. HierarchalClusteringPlot.png
+	4. KmeansClusteringPlot.png
---
+++ textmining.py
@@ -0,0 +1,111 @@
+import glob
+import pandas as pd 
+import string
+import os
+import matplotlib.pyplot as plt
+from os.path import isfile, join
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import MDS
+from sklearn.feature_extraction.text import CountVectorizer
+from yellowbrick.text import FreqDistVisualizer
+from wordcloud import WordCloud
+from scipy.cluster.hierarchy import ward, dendrogram
+
+filelist = glob.glob(os.path.join(os.getcwd(), "texts", "*.txt"))
+data = []
+for text in filelist:
+    with open(text) as f:
+        txt = f.read()
+        
+    data.append(txt)
+
+def word_process(text, stem = True) :
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    text = text.translate(table)
+    tokens = word_tokenize(text)
+    # remove all tokens that are not alphabetic
+    tokens= [word for word in tokens if word.isalpha()]
+    stopwords = nltk.corpus.stopwords.words('english')
+    tokens = [w for w in tokens if not w in stopwords]
+    if stem:
+        stemmer = SnowballStemmer("english")
+        tokens = [stemmer.stem(word) for word in tokens]
+    return tokens
+
+#define vectorizer parameters
+tfidf_vectorizer = TfidfVectorizer( tokenizer= word_process,
+                                    max_df=0.5,
+                                    min_df=0.1, stop_words='english',
+                                    use_idf=True)
+vectorizer = CountVectorizer(tokenizer=word_process,
+                            max_df = 0.5,
+                            min_df = 0.1, 
+                            stop_words = 'english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(data) 
+
+#fit the vectorizer to corpus data
+
+tf_model = vectorizer.fit_transform(data)
+total_tf = [sum(x) for x in zip(*tf_model.toarray())]
+terms = tfidf_vectorizer.get_feature_names()
+freq = []
+term_list = []
+popTerm = []
+pt = []
+cloud = {}
+for i,v in enumerate(total_tf):
+    if v > 35:
+        freq.append(v)
+        popTerm.append(i)
+        term_list.append(terms[i])
+        cloud[terms[i]] = v
+        pt.append(tf_model.transpose().toarray()[i])
+
+#frequency chart
+visualizer = FreqDistVisualizer(features=terms, orient='v')
+visualizer.fit(tf_model)
+visualizer.show()
+
+wordcloud = WordCloud(normalize_plurals= False).generate_from_frequencies(cloud)    
+
+# Display the generated wordcloud image:
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
+#k -means clustering
+km = KMeans(n_clusters= 2)
+km = km.fit(tf_model.transpose())
+print(tf_model.shape)
+clusters = km.labels_.tolist()
+
+#display the results
+color = [ '#d95f02' if x == 0 else '#7570b3' for x in clusters]
+figure, ax = plt.subplots(figsize=(20,15))
+for i in popTerm:
+    ax.scatter(tf_model.toarray()[2][i], tf_model.toarray()[3][i], c =color[i])
+    ax.annotate(terms[i], (tf_model.toarray()[2][i], tf_model.toarray()[3][i]))
+    print(terms[i])
+plt.show()
+
+#hierarchical clustering
+dist = euclidean_distances(pt)
+link_met = ward(dist)
+
+#visualize the results
+figure, ax = plt.subplots(figsize=(20,15))
+ax = dendrogram(link_met, orientation='top', labels= term_list)
+plt.tick_params(\
+    axis= 'x',
+    which='both',
+    bottom='off',
+    top='off',
+    labelbottom='off')
+plt.tight_layout()
+plt.show()
